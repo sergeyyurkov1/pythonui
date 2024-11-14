d201d00fa395d3917be00b75e2511d80c9f487c0
Yurkov Sergey <yurkov_sergey@outlook.com>
2024-11-15 04:12:01 +0800

    commit

--- screens/apps/settings/commands.py
+++ screens/apps/settings/commands.py
@@ -1,11 +1,15 @@
 from textual.containers import Container, Horizontal
 from textual.widgets import Static, Switch
 
+import functions
+
 layout = [
     Container(
         Horizontal(
             Static("Retro effects", classes="commands-static"),
-            Switch(animate=False),
+            Switch(
+                value=functions.read_yaml().get("retro_effects", False), animate=False
+            ),
             classes="commands-horizontal",
         ),
         classes="commands-container",

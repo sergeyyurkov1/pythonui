8166967fd515633646ce8cbf154aed5cf72d0f97
Yurkov Sergey <yurkov_sergey@outlook.com>
2024-11-16 23:26:41 +0800

    commit

--- git-push.bat
+++ git-push.bat
@@ -5,6 +5,7 @@ call git add --chmod=+x ./goodies/mhy
 call git add --chmod=+x ./goodies/weather
 call git add --chmod=+x ./goodies/sw
 call git add --chmod=+x ./goodies/test/01
+call git add --chmod=+x ./goodies/test/02
 
 call git add *
 

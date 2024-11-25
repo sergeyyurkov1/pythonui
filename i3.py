181d12d4013daecf32aaf90c6f5fa4631b46cc56
Yurkov Sergey <yurkov_sergey@outlook.com>
2024-11-26 04:36:36 +0800

    commit

--- i3.py
+++ i3.py
@@ -5,11 +5,11 @@ import functions
 
 
 def main():
-    def on_window_new(i3, e):
-        if e.container.name == "Terminal":
-            functions.log_to_txt(e.container.name)
-            # pyperclip.copy("~/pythonui/pos-greeter.py\n")
-            # pyperclip.paste()
+    # def on_window_new(i3, e):
+    #     if e.container.name == "Terminal":
+    #         functions.log_to_txt(e.container.name)
+    #         # pyperclip.copy("~/pythonui/pos-greeter.py\n")
+    #         # pyperclip.paste()
 
     def on_window_close(i3, e):
         tree = i3.get_tree()
@@ -17,14 +17,12 @@ def main():
         if len(tree.find_classed("terminal")) == 0:
             functions.log_to_txt("All terminal windows closed")
 
-            i3.command(
-                "exec xfce4-terminal --hide-menubar --hide-toolbar --hide-scrollbar"
-            )
+            i3.command("exec alacritty")
 
     i3 = i3ipc.Connection()
 
     i3.on(Event.WINDOW_CLOSE, on_window_close)
-    i3.on(Event.WINDOW_NEW, on_window_new)
+    # i3.on(Event.WINDOW_NEW, on_window_new)
 
     i3.main()
 
